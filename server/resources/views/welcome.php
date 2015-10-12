26ec309211100a0f8d111a15d819b31812db761b
at15 <at15@outlook.com>
2015-10-12 18:16:06 +0800

    Add angular summer note

---
+++ server/resources/views/welcome.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MissAtomicBomb</title>
+    <link rel="stylesheet" href="/assets/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="/assets/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="/assets/summernote/dist/summernote.css">
+</head>
+<body ng-app="example">
+<h1>editor</h1>
+
+<div ng-controller="MainCtrl as main">
+    <!--FIXME: content inside directive won't use  bind value -->
+    <summernote>{{main.dummy}}</summernote>
+    <summernote></summernote>
+</div>
+<!--<div id="summernote">You gonna miss me when I am gone</div>-->
+
+<script src="/assets/jquery/dist/jquery.js"></script>
+<script src="/assets/bootstrap/dist/js/bootstrap.js"></script>
+<script src="/assets/summernote/dist/summernote.js"></script>
+<script src="/assets/summernote/lang/summernote-zh-CN.js"></script>
+
+<script src="/assets/angular/angular.js"></script>
+<!--<script src="/assets/angular-bootstrap/ui-bootstrap-tpls.js"></script>-->
+<script src="/assets/angular-summernote/dist/angular-summernote.js"></script>
+<script src="/assets/javascript/bundle.js"></script>
+<script src="/assets/javascript/tmpl.js"></script>
+<script>
+    //    $(document).ready(function () {
+    //        $('#summernote').summernote({
+    //            lang: 'zh-CN',
+    //            onImageUpload: function (files) {
+    //                console.log('need to upload ');
+    //                // so the image won't got inserted
+    //            }
+    //        });
+    //    });
+
+</script>
+</body>
+</html>
--- src/script/app.js
+++ src/script/app.js
@@ -1,1 +1,6 @@
 // this is app.js
+angular.module('example', ['summernote'])
+    .controller('MainCtrl', function () {
+        var ctrl = this;
+        ctrl.dummy = "this is a dummy message";
+    });
