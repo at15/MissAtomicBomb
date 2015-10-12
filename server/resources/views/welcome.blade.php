d27ec2f84959b149b9e8c1a763d37783f0ff4f2f
at15 <at15@outlook.com>
2015-10-12 16:48:56 +0800

    Init summernote
    add all the assets directly

--- server/resources/views/welcome.blade.php
+++ server/resources/views/welcome.blade.php
@@ -2,8 +2,22 @@
 <html>
     <head>
         <title>MissAtomicBomb</title>
+        <link rel="stylesheet" href="/assets/bootstrap/dist/css/bootstrap.css">
+        <link rel="stylesheet" href="/assets/font-awesome/css/font-awesome.css">
+        <link rel="stylesheet" href="/assets/summernote/dist/summernote.css">
     </head>
     <body>
-        <h1>upload</h1>
+        <h1>editor</h1>
+        <div id="summernote">You gonna miss me when I am gone</div>
+
+        <script src="/assets/jquery/dist/jquery.js"></script>
+        <script src="/assets/bootstrap/dist/js/bootstrap.js"></script>
+        <script src="/assets/summernote/dist/summernote.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#summernote').summernote();
+            });
+
+        </script>
     </body>
 </html>
